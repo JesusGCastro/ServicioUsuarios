b868c5eb71804706f8e1cecf197138e4e893b6ca
JesusGCastro <renecast8@gmail.com>
2025-11-06 21:32:26 -0700

    Actualizacion del modelo usuario
    Se modifico el modelo del usuairo, ya que se tenia nombre como unico.

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ import sequelize from '../config/db.js';
 
 const User = sequelize.define('User', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  nombre: { type: DataTypes.STRING, allowNull: false, unique: true },
+  nombre: { type: DataTypes.STRING, allowNull: false },
   correo: { type: DataTypes.STRING, allowNull: false, unique: true },
   contrasenia: { type: DataTypes.STRING, allowNull: false },
 
